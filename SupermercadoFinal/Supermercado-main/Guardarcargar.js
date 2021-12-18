1f1630ae9ad07ab304ce1b7ebc629e79c945ef59
ritta83 <74908877+ritta83@users.noreply.github.com>
2021-12-18 14:45:55 +0100

    cambios
    cambios

---
+++ SupermercadoFinal/Supermercado-main/Guardarcargar.js
@@ -0,0 +1,77 @@
+
+
+class Guardarcargar{
+
+    constructor() {
+
+        
+    }
+      
+    guardarDatos() {
+
+        let datos_caja1 = document.getElementById("CAJA1");  
+        let datos_caja2 = document.getElementById("CAJA2");  
+        let datos_caja3 = document.getElementById("CAJA3");
+        let datos_caja4 = document.getElementById("CAJA4");
+    
+        //let cajasId = [datos_caja1.id, datos_caja2.id, datos_caja3, datos_caja4];
+        
+        let cajaValue1 = datos_caja1.value;  
+        let cajaValue2 = datos_caja2.value;
+        let cajaValue3 = datos_caja3.value;
+        let cajaValue4 = datos_caja4.value;
+    
+        let cajasValue_json = {"caja1" : cajaValue1, "caja2" : cajaValue2, "caja3" : cajaValue3, "caja4" : cajaValue4};
+    
+        let cajasValue_str = JSON.stringify(cajasValue_json);
+    
+        localStorage.setItem("cajas", cajasValue_str);
+    
+    }
+    
+    cargarMercado() {
+        
+    
+        console.log("pasa por 1");
+    
+        let cajasValue_str = localStorage.getItem("cajas");
+        let cajasValue_json = JSON.parse(cajasValue_str);
+        
+        console.log( cajasValue_json.caja1 );
+        console.log( cajasValue_json["caja1"] );
+    
+        let contador = 1;
+    
+        for (let key in cajasValue_json) {
+    
+            let value = cajasValue_json[key];
+    
+            console.log("mostramos el atributo con la clave " + key);
+            console.log(value);
+    
+            let divcaja = document.getElementById("divcaja" + contador); // cambiar atributos HTML / CSS
+            divcaja.style.background = "rgba(34,193,195,1)";
+    
+            let div_mensaje = document.getElementById(`mensaje` + contador);
+            div_mensaje.innerHTML = "<br>En la caja hay " + value + " cliente";
+    
+            let datos_caja = document.getElementById("CAJA" + contador);  
+            datos_caja.value = value;
+    
+            if (cajasValue_json[key] <= 0) {
+                //total_clientes_caja = 0;
+                divcaja.style.background = "rgba(131,58,180,1)";
+                div_mensaje.innerHTML = "<br>Caja cerrada";
+            } 
+    
+            contador++;
+        }
+    
+    }
+
+} 
+
+let guardarCargar = new Guardarcargar();//OBJETO GUARDARCARGAR
+
+
+
---
+++ SupermercadoFinal/Supermercado-main/supermercadocajas.js
@@ -0,0 +1,247 @@
+
+var inicio;
+
+
+let body = document.querySelector("#body"); // cambiar atributos HTML / CSS
+var style_body = window.getComputedStyle(body); // obtener atributos CSS 
+var rutaImagenFondoOriginal = style_body.getPropertyValue("background-image");
+
+// hacer dos botones uno paraguardar y otro para recuperar, usando web storage para que guarde como van las cajas
+
+function cambiarClientes(tipo_accion,numeroCaja) {
+
+    // 1 varaibles 2 condiciones etc
+
+    let input_caja;
+    let divcaja;
+    let stylecaja;
+    let mensajes;
+
+    if (numeroCaja == "caja1") {
+
+        let input_caja1 = document.getElementById("CAJA1");  
+        input_caja = input_caja1;
+        
+
+        let divcaja1 = document.querySelector("#divcaja1"); // cambiar atributos HTML / CSS
+        divcaja = divcaja1;
+        var style_divcaja1 = window.getComputedStyle(divcaja1); // obtener atributos CSS 
+        stylecaja = style_divcaja1;
+
+        mensajes = document.getElementById('mensaje1');
+
+        
+
+    } else if (numeroCaja == "caja2") {    
+
+        let input_caja2 = document.getElementById("CAJA2");
+        input_caja = input_caja2;
+
+        let divcaja2 = document.querySelector("#divcaja2");
+        divcaja = divcaja2;
+        var style_divcaja2 = window.getComputedStyle(divcaja2);
+        stylecaja = style_divcaja2;
+        
+        let div_mensaje = document.getElementById('mensaje2');
+        mensajes = div_mensaje;
+        
+        
+
+    } else if (numeroCaja == "caja3") {    
+
+        let input_caja3 = document.getElementById("CAJA3");
+        input_caja = input_caja3;
+
+        let divcaja3 = document.querySelector("#divcaja3");
+        divcaja = divcaja3;
+        var style_divcaja3 = window.getComputedStyle(divcaja3);
+        stylecaja = style_divcaja3;
+
+        let div_mensaje = document.getElementById('mensaje3');
+        mensajes = div_mensaje;
+
+    } else if (numeroCaja == "caja4") {    
+
+        let input_caja4 = document.getElementById("CAJA4");
+        input_caja = input_caja4;
+
+        let divcaja4 = document.querySelector("#divcaja4");
+        divcaja = divcaja4;
+        var style_divcaja4 = window.getComputedStyle(divcaja4);
+        stylecaja = style_divcaja4;
+
+        let div_mensaje = document.getElementById('mensaje4');
+        mensajes = div_mensaje;
+
+    }
+         
+    // esto aumentaría en uno el valor del input de caja1:
+    let total_clientes_caja = parseInt(input_caja.value);
+
+    
+    // consultando un VALOR CSS de un etiqueta:
+    console.log("|" + stylecaja.getPropertyValue("background") + "|");
+
+
+
+    // vamos a comprobar si el background de la caja está en verde
+
+    console.log("|" + stylecaja.getPropertyValue("background") + "|");
+    if (stylecaja.getPropertyValue("background") == "rgb(34, 193, 195) none repeat scroll 0% 0% / auto padding-box border-box") {
+        inicio = new Date();
+
+
+        // dependiendo de lo que pasemos en la variable "tipo_accion", sumará o restará:
+        if (tipo_accion == "poner") {
+            total_clientes_caja = total_clientes_caja + 1;
+            let mensaje_caja = mensajes;
+            console.log("holaaa" + mensaje_caja);
+            mensaje_caja.innerHTML = "<br>En la caja hay " + total_clientes_caja + " cliente";
+
+            
+        } else if (tipo_accion == "quitar") {
+            total_clientes_caja = total_clientes_caja - 1;
+            let mensaje_caja = mensajes;
+            mensaje_caja.innerHTML = "<br>En la caja hay " + total_clientes_caja + " cliente";
+
+          
+            
+            if (total_clientes_caja <= 0) {
+                total_clientes_caja = 0;
+                divcaja.style.background = "rgba(131,58,180,1)";
+                mensaje_caja.innerHTML = "<br>Caja cerrada";
+            } 
+            
+            
+        } else {
+            alert("has introducido mal el tipo de acción!");
+        }
+
+        input_caja.value = total_clientes_caja;
+
+    } else {
+
+        // cambiando un VALOR CSS de una etiqueta:
+        if (total_clientes_caja != 0 || tipo_accion == 'poner') {
+            console.log("vamos a poner el elemento en verde!");
+            divcaja.style.background = "rgba(34,193,195,1)";
+            let mensaje_caja = mensajes;
+            mensaje_caja.innerHTML = "<br>Caja abierta";
+
+            
+        }
+
+        // para añadir una clase nueva tenemos esto:
+        //divcaja1.setAttribute("class", "caja cajadisponible");
+
+    }
+        console.log(total_clientes_caja);
+
+
+   
+
+    if (total_clientes_caja == 4) {
+        alert("La caja esta saturada.");
+        let fin = new Date();
+        let tiempoTranscurrido = (fin-inicio)/1000; //en milisegundos
+        alert('tiempo transcurrido:' + tiempoTranscurrido);
+    }
+
+   
+}
+
+let input_caja1 = document.getElementById("CAJA1");
+        input_caja = input_caja1;
+
+
+        let divcaja1 = document.querySelector("#divcaja1"); // cambiar atributos HTML / CSS
+        divcaja = divcaja1;
+        var style_divcaja1 = window.getComputedStyle(divcaja1); // obtener atributos CSS 
+        stylecaja = style_divcaja1;
+
+        mensajes = document.getElementById('mensaje1');
+
+        console.log("|" + stylecaja.getPropertyValue("background") + "|");
+
+document.getElementById("divcaja1").oncontextmenu = function() { 
+
+    cambiarClientes('quitar', 'caja1'); 
+
+};
+
+document.getElementById("divcaja2").oncontextmenu = function() { 
+
+    cambiarClientes('quitar', 'caja2'); 
+
+};
+
+document.getElementById("divcaja3").oncontextmenu = function() { 
+
+    cambiarClientes('quitar', 'caja3'); 
+
+};
+
+document.getElementById("divcaja4").oncontextmenu = function() { 
+
+    cambiarClientes('quitar', 'caja4'); 
+
+};
+
+
+
+//FUNCIONALIDAD BOTON DIA Y NOCHE
+
+
+  function ponerOscuro(){
+
+    let body = document.getElementById('body'); // cambiar atributos HTML / CSS
+    
+    var style_body = window.getComputedStyle(body); // obtener atributos CSS 
+    
+    console.log(style_body.getPropertyValue("background-image"));
+
+      
+    if(style_body.getPropertyValue("background-image") == rutaImagenFondoOriginal){
+
+        let body = document.getElementById('body'); // cambiar atributos HTML / CSS
+        body.style.backgroundImage='url(noche1.jpg)';//QUITAR ESTA IMAGEN
+        
+    } else {
+
+        body.style.backgroundImage = 'url(super2.jpeg)';//imagen de fondo
+
+    }  
+
+  }
+
+  // BOTON BORRAR FONDO
+    function borrarFondo() {
+        
+        let body = document.getElementById('body'); // cambiar atributos HTML / CSS
+        
+        var style_body = window.getComputedStyle(body); // obtener atributos CSS 
+        
+        console.log(style_body.getPropertyValue("background-image"));
+        console.log(body.style.backgroundImage);
+        
+        // comprobamos si la imagen original está activa / mostrándose todavía:
+        if(style_body.getPropertyValue("background-image") == rutaImagenFondoOriginal){
+
+            body.style.backgroundImage='url()';
+
+        } else {
+
+            body.style.backgroundImage = 'url(super2.jpeg)';
+        }  
+        
+                   
+    }
+
+
+
+
+
+
+
+
+  
